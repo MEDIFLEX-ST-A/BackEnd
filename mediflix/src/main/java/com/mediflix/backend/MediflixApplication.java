00aef6e06af810fef39db8c0ffe6044e43c9b328
seeunjang <wisjang1@naver.com>
2023-03-12 04:10:27 +0900

    [feat] 초기 세팅

---
+++ mediflix/src/main/java/com/mediflix/backend/MediflixApplication.java
@@ -0,0 +1,13 @@
+package com.mediflix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediflixApplication.class, args);
+    }
+
+}
---
+++ mediflix/src/test/java/com/mediflix/backend/MediflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediflix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
